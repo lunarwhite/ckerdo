f742e7dfa841139e3c1248390f780b4c6ea8344b
Yuedong Wu <dwcn22@outlook.com>
2022-11-04 00:00:02 +0000

    Refactor: Separate algo names to const value

---
+++ balancer/const.go
@@ -0,0 +1,9 @@
+package balancer
+
+const (
+	R2Balancer             = "round-robin"
+	RandomBalancer         = "random"
+	IPHashBalancer         = "ip-hash"
+	ConsistentHashBalancer = "consistent-hash"
+	LeastLoadBalancer      = "least-load"
+)
