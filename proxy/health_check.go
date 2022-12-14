ecd4c7fead38d55146de9282c562340cb81156c8
Yuedong Wu <dwcn22@outlook.com>
2022-12-14 00:00:02 +0000

    Feat: Support health check for server

---
+++ proxy/health_check.go
@@ -0,0 +1,61 @@
+package proxy
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+// ReadAlive reads the alive status of the site
+func (h *HTTPProxy) ReadAlive(url string) bool {
+	h.RLock()
+	defer h.RUnlock()
+	return h.alive[url]
+}
+
+// SetAlive sets the alive status to the site
+func (h *HTTPProxy) SetAlive(url string, alive bool) {
+	h.Lock()
+	defer h.Unlock()
+	h.alive[url] = alive
+}
+
+// HealthCheck enable a health check goroutine for each agent
+func (h *HTTPProxy) HealthCheck(interval uint) {
+	for host := range h.hostMap {
+		go h.healthCheck(host, interval)
+	}
+}
+
+func (h *HTTPProxy) healthCheck(host string, interval uint) {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	for range ticker.C {
+		if !IsBackendAlive(host) && h.ReadAlive(host) {
+			log.Printf("Site unreachable, remove %s from load balancer.", host)
+
+			h.SetAlive(host, false)
+			h.lb.Remove(host)
+		} else if IsBackendAlive(host) && !h.ReadAlive(host) {
+			log.Printf("Site reachable, add %s to load balancer.", host)
+
+			h.SetAlive(host, true)
+			h.lb.Add(host)
+		}
+	}
+}
+
+// IsBackendAlive attempt to establish a tcp connection to check whether the site is alive
+func IsBackendAlive(host string) bool {
+	addr, err := net.ResolveTCPAddr("tcp", host)
+	if err != nil {
+		return false
+	}
+	resolveAddr := fmt.Sprintf("%s:%d", addr.IP, addr.Port)
+	conn, err := net.DialTimeout("tcp", resolveAddr, ConnectionTimeout)
+	if err != nil {
+		return false
+	}
+	_ = conn.Close()
+	return true
+}
